a1e2b3c56ffa79280ca54e4b3754a4571bdb6a6c
chadi.laoulaou <chadilaoulaou@gmail.com>
2023-10-12 22:34:03 +0200

    My first action is ready

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -7,18 +6,18 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const ms: string = core.getInput('resource')
+    const ms1: string = core.getInput('method')
+    const ms2: string = core.getInput('flags')
 
+    const o: string = `Hello ${ms} ${ms1} ${ms2}`
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(o)
 
     // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('result', o)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
